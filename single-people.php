b2bf5e38ae3035f860d93779eb4f897f4218e6f6
julia panek <jcpanek@gmail.com>
2014-01-10 09:35:52 -0500

    consistent spacing in menu, fixed bullets clipping, cv styling

--- single-people.php
+++ single-people.php
@@ -2,35 +2,6 @@
 
 <div class="content col-xs-12 col-sm-5 col-sm-push-7 col-lg-6 col-lg-push-6">
 	
-		<div class="resume">
-<?php
-		if (get_field('resume')) {
-				echo '<ul>';
-				 
-				while (has_sub_field('resume')) {
-					echo '<li><p class="location">' . get_sub_field('section_title') . '</p><br>';
-
-					if (get_sub_field('section_jobs')) {
-						echo '<ul>';
-				 
-					   	while (has_sub_field('section_jobs')) {
-							echo '<li class="noindent">' . get_sub_field('name') . get_sub_field('description') . '</li>';
-						}
-						
-						echo '</ul>';
-					}
-					
-					echo '</li>';				   
-			   }
-			   
-				echo '</ul>';
-			}
-?>
-		</div>
-</div>
-
-<div class="content col-xs-12 col-sm-7 col-sm-pull-5 col-lg-6 col-lg-pull-6">
-
 		<p class="title"><?php the_field('name'); ?></p>
 		<h1 class="location"><?php the_field('role'); ?></h1>
 		<h1 class="location"><?php the_field('license'); ?></h1>
@@ -43,7 +14,35 @@
 		}
 ?>
 		<p class="normal"><?php the_field('bio'); ?></p>
+</div>
 
+<div class="content col-xs-12 col-sm-7 col-sm-pull-5 col-lg-6 col-lg-pull-6">
+
+		<div class="resume">
+<?php
+		if (get_field('resume')) {
+				echo '<ul>';
+				 
+				while (has_sub_field('resume')) {
+					echo '<li class="resume"><p class="resume_section">' . get_sub_field('section_title') . '</p><br>';
+
+					if (get_sub_field('section_jobs')) {
+						echo '<ul>';
+				 
+					   	while (has_sub_field('section_jobs')) {
+							echo '<li class="noindent">' . get_sub_field('name') . '</li><li class="indent">' . get_sub_field('description') . '</li>';
+						}
+						
+						echo '</ul>';
+					}
+					
+					echo '</li>';				   
+			   }
+			   
+				echo '</ul>';
+			}
+?>
+		</div>
 </div>
 
 <?php get_footer(); ?>
