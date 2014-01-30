48de1ecd933ef2a854bfaf47ee27793ea5711fe9
Bill Boyer <william.boyer.nyc@gmail.com>
2014-01-30 16:30:36 -0500

    Header logos click through to homepage.

--- header.php
+++ header.php
@@ -25,16 +25,16 @@
 <div id="container" class="container">
 
 <div id="header_content">
-	<img class="logo" src="<?php bloginfo('template_directory'); ?>/img/leslie-gill-architect.png" alt="leslie gill architect">
+	<a href="/"><img class="logo" src="<?php bloginfo('template_directory'); ?>/img/leslie-gill-architect.png" alt="leslie gill architect"></a>
 	<div class="row">
 		<div class="col-xs-12 col-lg-2"></div>
 		<div class="col-xs-12 col-lg-10">
 			<div class="row">
 				<div id="header-column-2" class="col-xs-7">
-					<img class="logo_left" src="<?php bloginfo('template_directory'); ?>/img/leslie-gill.png" alt="leslie gill architect">
+					<a href="/"><img class="logo_left" src="<?php bloginfo('template_directory'); ?>/img/leslie-gill.png" alt="leslie gill architect"></a>
 				</div>
 				<div id="header-column-3" class="col-xs-5">
-					<img class="logo_right" src="<?php bloginfo('template_directory'); ?>/img/architect.png" alt="leslie gill architect">
+					<a href="/"><img class="logo_right" src="<?php bloginfo('template_directory'); ?>/img/architect.png" alt="leslie gill architect"></a>
 				</div>
 			</div>
 		</div>
